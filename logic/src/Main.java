d2f4d08044ad625514d14edd34cb8d21ea498386
Eric Separovic <ericseparovic07@gmail.com>
2024-03-12 16:57:21 -0300

    create method add new book

--- logic/src/Main.java
+++ logic/src/Main.java
@@ -1,21 +1,36 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
        Library library = new Library();
        Scanner scanner = new Scanner(System.in);
-       
-       int choice = -1;
+       boolean run= false;
 
-       while(choice != 0){
-           choice = displayMenu(scanner);
-           scanner.nextLine();
 
-           switch (choice){
-               case 1:
-                   addBook(library, scanner);
+       while(!run){
+           try {
+               int choice = displayMenu(scanner);
+               scanner.nextLine();
+
+               switch (choice){
+                  case 1:
+                      addBook(library, scanner);
+                  break;
+                   case 0:
+                       run = false;
                    break;
+                }
+
+
+           } catch (InputMismatchException e) {
+               System.out.println("Invalid Choice!");
+               scanner.nextLine();
            }
+
+
+
        }
        
         
@@ -24,17 +39,58 @@ public class Main {
     }
 
     public static void addBook(Library library, Scanner scanner){
-        System.out.println("Enter title: ");
-        String title = scanner.nextLine();
-        System.out.println("Enter author: ");
-        String author = scanner.nextLine();
-        System.out.println("Enter genre: ");
-        String genre = scanner.nextLine();
-        System.out.println("Enter publication Year: ");
-        int publicationYear = scanner.nextInt();
+
+        String title = "";
+        String author = "";
+        String genre = "";
+        int publicationYear = 0;
+
+
+        while (title.trim().isEmpty()){
+            System.out.println("Enter title: ");
+            title = scanner.nextLine().trim();
+
+            if(title.isEmpty()){
+                System.out.println("Title cannot be empty. Please try again.");
+            }
+        }
+
+        while (author.trim().isEmpty()){
+            System.out.println("Enter author: ");
+            author = scanner.nextLine().trim();
+
+            if(author.isEmpty()){
+                System.out.println("Author cannot be empty. Please try again.");
+            }
+        }
+
+        while (genre.trim().isEmpty()){
+            System.out.println("Enter genre: ");
+            genre = scanner.nextLine().trim();
+            if(genre.isEmpty()){
+                System.out.println("Genre cannot be empty. Please try again.");
+            }
+        }
+
+        boolean invalidInput = false;
+        while(!invalidInput){
+            try {
+                System.out.println("Enter publication year: ");
+                publicationYear = scanner.nextInt();
+                scanner.nextLine();
+                invalidInput = true;
+            } catch (Exception e){
+                System.out.println("Invalid type data. Please try again.");
+                scanner.nextLine();
+            }
+        }
 
         Book book = new Book(title, author, genre, publicationYear);
         library.addBook(book);
+        
+       
+;
+library.addBook(book);
     }
 
     public static int displayMenu(Scanner scanner){
