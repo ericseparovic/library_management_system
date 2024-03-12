f717a9c46f05326c41603190fa286995a27cb560
Eric Separovic <ericseparovic07@gmail.com>
2024-03-12 16:25:49 -0300

    create method add new book

--- logic/src/Main.java
+++ logic/src/Main.java
@@ -9,6 +9,13 @@ public class Main {
 
        while(choice != 0){
            choice = displayMenu(scanner);
+           scanner.nextLine();
+
+           switch (choice){
+               case 1:
+                   addBook(library, scanner);
+                   break;
+           }
        }
        
         
@@ -16,8 +23,22 @@ public class Main {
        
     }
 
+    public static void addBook(Library library, Scanner scanner){
+        System.out.println("Enter title: ");
+        String title = scanner.nextLine();
+        System.out.println("Enter author: ");
+        String author = scanner.nextLine();
+        System.out.println("Enter genre: ");
+        String genre = scanner.nextLine();
+        System.out.println("Enter publication Year: ");
+        int publicationYear = scanner.nextInt();
+
+        Book book = new Book(title, author, genre, publicationYear);
+        library.addBook(book);
+    }
+
     public static int displayMenu(Scanner scanner){
-        System.out.println("Choose an option: ");
+        System.out.println("\nChoose an option: ");
         System.out.println("1: Add new Book");
         System.out.println("2: Search book for title");
         System.out.println("3: Search book for author");
